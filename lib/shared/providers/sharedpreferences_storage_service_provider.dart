45f2c1890888ce2fe77c039d516675af3fcee854
yabuuchi <yabuuchi@merxevo.co.jp>
2023-11-07 09:57:07 +0900

    LocalStorageへの保存方法変更

--- lib/shared/providers/sharedpreferences_storage_service_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_project/shared/data/local/shared_prefs_storage_service.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final storageServiceProvider = Provider((ref) {
-  final SharedPrefsService prefsService = SharedPrefsService();
-  prefsService.init();
-  return prefsService;
-});
