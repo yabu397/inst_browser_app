20167a23b48e8faaeb6cc9f62738e09dd88a483b
yabuuchi <yabuuchi@merxevo.co.jp>
2023-11-10 10:02:26 +0900

    エラーチェック追加

--- lib/features/bookmark/presentation/widgets/bookmark_form_dialog.dart
+++ lib/features/bookmark/presentation/widgets/bookmark_form_dialog.dart
@@ -3,8 +3,9 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_project/features/bookmark/presentation/providers/bookmark_state_provider.dart';
 import 'package:flutter_project/shared/globals.dart';
 import 'package:flutter_project/shared/widgets/cst_snack_bar.dart';
-import 'package:flutter_project/shared/widgets/cst_text_field.dart';
+import 'package:flutter_project/shared/widgets/cst_text_form_field.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class BookmarkFormDialog extends ConsumerStatefulWidget {
   const BookmarkFormDialog({Key? key, required this.mode}) : super(key: key);
@@ -15,6 +16,7 @@ class BookmarkFormDialog extends ConsumerStatefulWidget {
 }
 
 class _BookmarkFormDialogState extends ConsumerState<BookmarkFormDialog> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(bookmarkNotifierProvider);
@@ -22,89 +24,108 @@ class _BookmarkFormDialogState extends ConsumerState<BookmarkFormDialog> {
     return Dialog(
       child: Padding(
         padding: Sizing.defaultInsets,
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Text(
-              switch (widget.mode) {
-                Mode.regist => L10n.of(context).bookmarkRegistDialogTitle,
-                Mode.update => L10n.of(context).bookmarkEditDialogTitle,
-                _ => ''
-              },
-              style: Style.boldStyle.copyWith(fontSize: Sizing.titleSize)),
-          CstTextField(
-            label: L10n.of(context).titleLabel,
-            onSubmitted: (String value) => notifier.setTitle(value),
-          ),
-          CstTextField(
-            label: L10n.of(context).urlLabel,
-            onSubmitted: (String value) => notifier.setUrl(value),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: Sizing.paddingSize,
-              bottom: Sizing.paddingSize,
+        child: Form(
+          key: _formKey,
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Text(
+                switch (widget.mode) {
+                  Mode.regist => L10n.of(context).bookmarkRegistDialogTitle,
+                  Mode.update => L10n.of(context).bookmarkEditDialogTitle,
+                  _ => ''
+                },
+                style: Style.boldStyle.copyWith(fontSize: Sizing.titleSize)),
+            CstTextFormField(
+              label: L10n.of(context).titleLabel,
+              onChanged: (String value) => notifier.setTitle(value),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  child: FractionallySizedBox(
-                    widthFactor: 0.8,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.orange,
-                      ),
-                      onPressed: () {
-                        notifier.fetchBookmarks();
-                      },
-                      child: Text(L10n.of(context).cancel),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: FractionallySizedBox(
-                    widthFactor: 0.8,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.blueAccent,
-                      ),
-                      onPressed: () {
-                        switch (widget.mode) {
-                          case Mode.regist:
-                            if (state.bookmarkList.length >=
-                                Limits.maxBookmarkCount) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  CstSnackBar(context,
-                                      text: L10n.of(context).maxBookmark(
-                                          Limits.maxBookmarkCount)));
-                              break;
-                            }
-                            notifier
-                                .insertBookmark(state.bookmark)
-                                .then((value) => notifier.fetchBookmarks());
-                            break;
-                          case Mode.update:
-                            notifier
-                                .updateBookmark(state.bookmark)
-                                .then((value) => notifier.fetchBookmarks());
-                            break;
-                        }
-                      },
-                      child: Text(
-                        switch (widget.mode) {
-                          Mode.regist => L10n.of(context).regist,
-                          Mode.update => L10n.of(context).update,
-                          _ => ''
+            CstTextFormField(
+                label: L10n.of(context).urlLabel,
+                onChanged: (String value) => notifier.setUrl(value),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return L10n.of(context).requiredText;
+                  }
+                  try {
+                    WebViewController().loadRequest(
+                      Uri.parse(value),
+                    );
+                  } catch (e) {
+                    return L10n.of(context).inputValidUrl;
+                  }
+                  return null;
+                }),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: Sizing.paddingSize,
+                bottom: Sizing.paddingSize,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: FractionallySizedBox(
+                      widthFactor: 0.8,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.orange,
+                        ),
+                        onPressed: () {
+                          notifier.fetchBookmarks();
                         },
+                        child: Text(L10n.of(context).cancel),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ]),
+                  Expanded(
+                    child: FractionallySizedBox(
+                      widthFactor: 0.8,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.blueAccent,
+                        ),
+                        onPressed: () {
+                          switch (widget.mode) {
+                            case Mode.regist:
+                              if (state.bookmarkList.length >=
+                                  Limits.maxBookmarkCount) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    CstSnackBar(context,
+                                        text: L10n.of(context).maxBookmark(
+                                            Limits.maxBookmarkCount)));
+                                break;
+                              }
+                              if (_formKey.currentState!.validate()) {
+                                notifier
+                                    .insertBookmark(state.bookmark)
+                                    .then((value) => notifier.fetchBookmarks());
+                              }
+                              break;
+                            case Mode.update:
+                              if (_formKey.currentState!.validate()) {
+                                notifier
+                                    .updateBookmark(state.bookmark)
+                                    .then((value) => notifier.fetchBookmarks());
+                              }
+                              break;
+                          }
+                        },
+                        child: Text(
+                          switch (widget.mode) {
+                            Mode.regist => L10n.of(context).regist,
+                            Mode.update => L10n.of(context).update,
+                            _ => ''
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
