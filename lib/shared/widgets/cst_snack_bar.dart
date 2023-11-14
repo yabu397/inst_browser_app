9a6776bc7fe7535589ae2a9109de128b89f74b45
yabuuchi <yabuuchi@merxevo.co.jp>
2023-11-14 10:58:19 +0900

    不必要なOptionalを削除

--- lib/shared/widgets/cst_snack_bar.dart
+++ lib/shared/widgets/cst_snack_bar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CstSnackBar extends SnackBar {
-  CstSnackBar(BuildContext context, {String? text, super.key})
+  CstSnackBar(BuildContext context, {required String text, super.key})
       : super(
-          content: Expanded(child: Text(text ?? '')),
+          content: Expanded(child: Text(text)),
           showCloseIcon: true,
           closeIconColor: Colors.white,
         );
